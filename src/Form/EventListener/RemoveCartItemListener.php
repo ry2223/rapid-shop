59d69a608bb4f5a25e9d155959e6639c7ca197c0
ry2223 <robyen22@gmail.com>
2022-10-19 14:06:34 +0200

    Add cart clear and remove functionality, add ability to remove expired carts

---
+++ src/Form/EventListener/RemoveCartItemListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\EventListener;
+
+use App\Entity\Order;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+class RemoveCartItemListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [FormEvents::POST_SUBMIT => 'postSubmit'];
+    }
+
+    public function postSubmit(FormEvent $event): void
+    {
+        $form = $event->getForm();
+        $cart = $form->getData();
+
+        if (!$cart instanceof Order) {
+            return;
+        }
+
+        // Removes items from the cart
+        foreach ($form->get('items')->all() as $child) {
+            if ($child->get('remove')->isClicked()) {
+                $cart->removeItem($child->getData());
+                break;
+            }
+        }
+    }
+}
